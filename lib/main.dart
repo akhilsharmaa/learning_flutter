23b64ab8f68b805147bade0f78ac57a58ab44c14
Akhil Sharma <akhil.console@gmail.com>
2021-02-06 20:22:45 +0530

    TabBar created With three tabs

--- lib/main.dart
+++ lib/main.dart
@@ -11,59 +11,23 @@ void main() {
 class CardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("CardView"),
-        leading: Icon(Icons.watch_rounded),
-      ),
-      body: Center(
-        child: SizedBox(
-          height: 490,
-          width: 400,
-          child: Container(
-            color: Colors.white,
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image(
-                    image: NetworkImage(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                  ),
-                ),
-                Container(
-                  child: Center(
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        "Ullu Decroz",
-                        style: TextStyle(fontSize: 25, color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  padding: const EdgeInsets.only(
-                      top: 3, bottom: 3, right: 10, left: 10),
-                ),
-                Container(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "I am very Bakwass and i am in the age of None",
-                      style: TextStyle(fontSize: 16, color: Colors.black45),
-                    ),
-                  ),
-                  padding: const EdgeInsets.only(
-                      top: 3, bottom: 10, right: 10, left: 10),
-                )
-              ],
-            ),
+    return DefaultTabController(
+      length: 3, // Initialize how many tabs you need
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("CardView"),
+          leading: Icon(Icons.watch_rounded),
+          bottom: TabBar(
+            // Create all the tabs
+            tabs: <Widget>[
+              Tab(text: "Hey",),
+              Tab(text: "Heys",),
+              Tab(text: "Heys",)
+            ],
           ),
         ),
+
       ),
-      backgroundColor: Colors.red
-      ,
     );
   }
 }
