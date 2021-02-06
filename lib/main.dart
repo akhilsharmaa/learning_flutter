bc3b636fedfd43affe52414100374c47f92fbe6b
Akhil Sharma <akhil.console@gmail.com>
2021-02-06 17:07:43 +0530

    Merge remote-tracking branch 'origin/card_view' into card_view

--- lib/main.dart
+++ lib/main.dart
@@ -4,55 +4,14 @@ import 'Contry.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ScaffoldApp(),)
+    home: CardView(),)
   );
 }
 
-  class ScaffoldApp extends StatelessWidget {
+  class CardView extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: AppBar(
-          title: Text("Scafold App"),
-        ),
-        body: Center(
-          child: Container(
-              padding: EdgeInsets.all(10),
-              child : Expanded(
-                child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.pinkAccent,
-                      child: Text("Hey i am here" ,style: TextStyle(
-                        fontSize: 24
-                      ),),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.amber,
-                      child: Text("Hey i am and this is wierd" ,style: TextStyle(
-                        fontSize: 24
-                      ),),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.deepOrange,
-                      child: Text("Hey i am here" ,style: TextStyle(
-                        fontSize: 24
-                      ),),
-                    ),
-                  ),
-                ],
-            ),
-              )
-          ),
-        ),
-      );
+      return Container();
     }
   }
+
