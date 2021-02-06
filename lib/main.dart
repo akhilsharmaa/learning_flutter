57813564726b34b15a0e46d5f50a7ba2317833f3
Akhil Sharma <akhil.console@gmail.com>
2021-02-06 18:06:29 +0530

    CardView Created with card Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,45 @@ import 'Contry.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: CardView(),)
-  );
+    home: CardView(),
+  ));
 }
 
-  class CardView extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Container();
-    }
+class CardView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CardView"),
+        leading: Icon(Icons.watch_rounded),
+      ),
+      body: Center(
+        child: CardingView(),
+      ),
+    );
   }
+}
 
+
+class CardingView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const ListTile(
+              leading: Icon(Icons.access_alarms_outlined),
+              title: Text("The Enhanced Hero of coding"),
+              subtitle: Text("I actually don't know what to say."),
+            )
+            ,Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
